610f9623fbe816f6f30be279e3b1887578d654da
David Staub <david_staub@hotmail.com>
2019-11-07 11:20:27 +0100

    Update test_word_analogy.py

--- tests/tasks/test_word_analogy.py
+++ tests/tasks/test_word_analogy.py
@@ -16,7 +16,11 @@ def test_word_analogy_should_fail_on_wrong_return_string():
     # GIVEN
     task = WordAnalogyTask()
     embedding_mock = MagicMock(name="embedding")
-    embedding_mock.keyed_vectors.most_similar_cosmul.return_value = {"some":0.89, "thing":0.45, "else":0.43}
+    embedding_mock.keyed_vectors.most_similar_cosmul.return_value = {
+        "some": 0.89,
+        "thing": 0.45,
+        "else": 0.43
+    }
 
     # WHEN
     report = task.evaluate(embedding_mock)
@@ -29,7 +33,11 @@ def test_word_analogy_should_pass_when_lannister_is_returned():
     # GIVEN
     task = WordAnalogyTask()
     embedding_mock = MagicMock(name="embedding")
-    embedding_mock.keyed_vectors.most_similar_cosmul.return_value =  {"some":0.89, "Lannister":0.45, "else":0.43}
+    embedding_mock.keyed_vectors.most_similar_cosmul.return_value = {
+        "some": 0.89,
+        "Lannister": 0.45,
+        "else": 0.43
+    }
 
     # WHEN
     report = task.evaluate(embedding_mock)
