813b8122699a8b1cfe5927e2687733cdf850ebb1
David Staub <david_staub@hotmail.com>
2019-11-07 11:11:05 +0100

    add tests for word_analogy task

---
+++ tests/tasks/test_word_analogy.py
@@ -0,0 +1,38 @@
+"""
+embedeval
+~~~~~~~~~
+
+NLP Embedding Evaluation Tool
+
+:copyright: (c) 2019 by David Staub <david_staub@hotmail.com>
+:license: MIT, see LICENSE for more details.
+"""
+
+from embedeval.tasks.word_analogy import WordAnalogyTask
+from unittest.mock import MagicMock
+
+
+def test_word_analogy_should_fail_on_wrong_return_string():
+    # GIVEN
+    task = WordAnalogyTask()
+    embedding_mock = MagicMock(name="embedding")
+    embedding_mock.keyed_vectors.most_similar_cosmul.return_value = {"some":0.89, "thing":0.45, "else":0.43}
+
+    # WHEN
+    report = task.evaluate(embedding_mock)
+
+    # THEN
+    assert not report.outcome
+
+
+def test_word_analogy_should_pass_when_lannister_is_returned():
+    # GIVEN
+    task = WordAnalogyTask()
+    embedding_mock = MagicMock(name="embedding")
+    embedding_mock.keyed_vectors.most_similar_cosmul.return_value =  {"some":0.89, "Lannister":0.45, "else":0.43}
+
+    # WHEN
+    report = task.evaluate(embedding_mock)
+
+    # THEN
+    assert report.outcome
